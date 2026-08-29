5e6c95fa216e58e5b7826d698776f38a12bf6326
Moritz <moritz.korth@uni-muenster.de>
2026-08-29 12:51:03 +0200

    Added Constructor für Sprite+ (Animation Timer)

--- src/model/Player.hpp
+++ src/model/Player.hpp
@@ -7,18 +7,21 @@
 
 class Player
 {
-private:
+    sf::Texture runningTexture;
+    sf::Sprite playerSprite;
     sf::RectangleShape playerShape;
     std::vector<sf::IntRect> running_frames;
 
     sf::Vector2f position;
-
     sf::Vector2f velocity;
 
     const Girder* current_girder;
 
     float playerHeight = 40.f;
     float playerWidth = 30.f;
+
+    float animationTimer;
+    int currentFrame;
 
     bool isJumping;
     bool isClimbing;
@@ -27,6 +30,7 @@ public:
     Player(sf::Vector2f position);
     ~Player();
 
+    sf::Sprite getSprite();
     sf::RectangleShape getShape();
     
     void move(Direction direction);
@@ -38,6 +42,4 @@ public:
     void check_girder_intersection(const std::vector<Girder>& girders);
     void setGirder(const Girder& girder);
     const Girder* getGirder() const;
-
 };
-
