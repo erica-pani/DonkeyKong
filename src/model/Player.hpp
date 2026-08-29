889ee4802774db7418b1156e1cef6d5542710633
Moritz <moritz.korth@uni-muenster.de>
2026-08-29 12:53:00 +0200

    Added missing Includes

--- src/model/Player.hpp
+++ src/model/Player.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
+#include <vector>
 #include "Ladder.hpp"
 #include "Girder.hpp"
 #include "Direction.hpp"
